05ea58b5dd1ccac3ecafd9da838beb995da4bef4
KevinYang528 <r05631027@ntu.edu.tw>
2017-11-17 23:48:00 +0800

    model to dropbox

--- hw3/hw3_test.sh
+++ hw3/hw3_test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-wget -O "my_model_0.66453.h5" "https://github.com/KevinYang528/ML2017FALL/releases/download/0.0.0/my_model_0.66453.h5"
-wget -O "my_model_0.66620.h5" "https://github.com/KevinYang528/ML2017FALL/releases/download/0.0.0/my_model_0.66620.h5"
-wget -O "my_model_0.67093.h5" "https://github.com/KevinYang528/ML2017FALL/releases/download/0.0.0/my_model_0.67093.h5"
-wget -O "my_model_0.67679.h5" "https://github.com/KevinYang528/ML2017FALL/releases/download/0.0.0/my_model_0.67679.h5"
-wget -O "my_model_0.67846.h5" "https://github.com/KevinYang528/ML2017FALL/releases/download/0.0.0/my_model_0.67846.h5"
+wget -O "my_model_0.66453.h5" "https://www.dropbox.com/s/ugt8xqoaptjj7cg/model_0.66453.h5?dl=1"
+wget -O "my_model_0.66620.h5" "https://www.dropbox.com/s/fuzg0ykndk7zokj/model_0.66620.h5?dl=1"
+wget -O "my_model_0.67093.h5" "https://www.dropbox.com/s/gcjng9byqawjyau/model_0.67093.h5?dl=1"
+wget -O "my_model_0.67679.h5" "https://www.dropbox.com/s/48i7l3844pqz5ge/model_0.67679.h5?dl=1"
+wget -O "my_model_0.67846.h5" "https://www.dropbox.com/s/krwpmomqmop1xtj/model_0.67846.h5?dl=1"
 python3 test.py $1 $2
