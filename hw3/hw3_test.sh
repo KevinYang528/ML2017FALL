29737634525269bb53d1d5084085395bc2c6add3
KevinYang528 <r05631027@ntu.edu.tw>
2017-11-17 23:38:38 +0800

    test.sh

--- hw3/hw3_test.sh
+++ hw3/hw3_test.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
+wget -O "my_model_0.66453.h5" "https://github.com/KevinYang528/ML2017FALL/releases/download/0.0.0/my_model_0.66453.h5"
+wget -O "my_model_0.66620.h5" "https://github.com/KevinYang528/ML2017FALL/releases/download/0.0.0/my_model_0.66620.h5"
+wget -O "my_model_0.67093.h5" "https://github.com/KevinYang528/ML2017FALL/releases/download/0.0.0/my_model_0.67093.h5"
+wget -O "my_model_0.67679.h5" "https://github.com/KevinYang528/ML2017FALL/releases/download/0.0.0/my_model_0.67679.h5"
+wget -O "my_model_0.67846.h5" "https://github.com/KevinYang528/ML2017FALL/releases/download/0.0.0/my_model_0.67846.h5"
 python3 test.py $1 $2
